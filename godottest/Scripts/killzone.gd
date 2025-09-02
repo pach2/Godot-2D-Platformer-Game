58f8ffde589e9f6f5a39efe27fc40d7b82f1dde1
pach2 <125647665+pach2@users.noreply.github.com>
2025-09-02 13:57:59 +0300

    Player animation and Game over sequence
    I added some animations for the player which include Idle, walk, and Jump. I also improved the death animations

--- godottest/Scripts/killzone.gd
+++ godottest/Scripts/killzone.gd
@@ -4,10 +4,13 @@ extends Area2D
 
 func _on_body_entered(_body: Node2D) -> void:
 	print("You Died")
+	Engine.time_scale = 0.5
+	_body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
